4b1c91625ceaa7579431ae633b14412a4e44fa40
Ronnie Ripley <ripleyronnie5@gmail.com>
2018-02-11 23:34:57 -0800

    Worked on ContestLab, Still need to finish

--- ContestLab.java
+++ ContestLab.java
@@ -12,8 +12,10 @@ public class ContestLab {
    
    public static void main (String args[]) throws FileNotFoundException
    {
-      //Scanner input = getInput(console);
 
+      PrintStream newF = new PrintStream(new File("report2017.txt"));
+      
+      displayReportHeading(newF);
    }
    
    //Gets a user input for the name of a file
@@ -31,8 +33,43 @@ public class ContestLab {
       return new Scanner(f);
    }
    
-   public static void displayReprotHeading(PrintStream output)
+   public static void displayReportHeading(PrintStream output) throws FileNotFoundException
    {
+      Scanner input = getInput(console);
+      displayHeader(output);
+      int count = 0;
+      double totalTime = 0;
+      double totalDistance = 0;
+      String currentBest = " ";
+      
+      System.out.print("=\n ");
+      output.print("=\n ");
+      while(input.hasNextLine())
+      {
+         count++;
+         String nextLine = input.nextLine();
+         System.out.printf(" %s", nextLine);
+         output.printf(" %s", nextLine);
+                 
+         while(input.hasNextDouble())
+         {
+            double next1 = input.nextDouble();
+            //gitraceWinner((nextLine
+            totalDistance += next1;
+            double next2 = input.nextDouble();
+            totalTime += next2;
+            for(int i=1; i<=33-(nextLine.length()); i++)
+            {
+               System.out.print(" ");
+               output.print(" ");
+            }
+            System.out.printf("%.2f ft     %.2f sec\n", next1, next2);
+            output.printf("%.2f ft     %.2f sec\n", next1, next2);
+         }
+      }
+      
+      printEquals(60,output);
+      summary(output,count/2+1,totalTime,totalDistance);
       
    }
    
@@ -67,8 +104,31 @@ public class ContestLab {
       }
    }
    
-   public static void summary(PrintStream output)
+   //Displayes the summary at the end 
+   public static void summary(PrintStream output, int count, double ttlTime, double ttlDistance)
    {
-   
+      System.out.print("\nTotal Number of Contestants: " + count);
+      System.out.printf("\nAverage race time: %.2f", (ttlTime/count));
+      System.out.printf("\nAverage toss distance: %.2f", (ttlDistance/count));
+      
+   }
+   //Displays the header that consisit of the Contestants, distance, and time titles
+   public static void displayHeader(PrintStream output)
+   {
+      printEquals(60,output);
+      
+      System.out.println("\n  Contestants                      Distance        Time");
+      output.println("\n  Contestants                      Distance        Time");
+      
+      printEquals(59,output);
+   }
+   //Displays a row of equal symbols to the amount given
+   public static void printEquals(int amount, PrintStream output)
+   {
+      for(int i=1; i<=amount; i++)
+      {
+         System.out.print("=");
+         output.print("=");
+      }
    }
 }
